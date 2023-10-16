82c93cfcf98a25b7c069adb0fb2e101e6f45b526
katsaGeorge <georgekatsabsn@gmail.com>
2023-10-16 20:54:11 +0300

    Template-Driven-Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { PersonAltComponent } from './person-alt/person-alt.component';
 import { EventBindComponent } from './event-bind/event-bind.component';
 import { OutputDemoComponent } from './output-demo/output-demo.component';
 import { PersonCardComponent } from './person-card/person-card.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, PersonComponent, PersonAltComponent, EventBindComponent, 
     OutputDemoComponent,
-    PersonCardComponent
+    PersonCardComponent,
+    TemplateDrivenFormComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -128,4 +130,9 @@ onSendUser(user: Person){
   console.log(user)
   this.sentUser = user;
 }
+
+onNewPerson(person: Person){
+  this.users.push(person);
+}
+
 }
